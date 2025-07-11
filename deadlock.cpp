3de4b93f0ba77fcea3ff6dcfb0c1595e3d59f73a
Rayhan Islam Prome <rayhanspydet@gmail.com>
2025-07-11 10:58:34 +0600

    Add files via upload

--- deadlock.cpp
+++ deadlock.cpp
@@ -1,54 +1,64 @@
 #include <iostream>
 using namespace std;
-
-#define MAX 100
+const int N = 10;
 
 int main() {
-    int n, m, Alloc[MAX][MAX], Max[MAX][MAX], Need[MAX][MAX], Avail[MAX], Finish[MAX] = {0}, SafeSeq[MAX];
+    int n, m, A[N][N], M[N][N], Need[N][N], Avail[N], Seq[N];
+    bool Finish[N] = {};
 
-    cout << "Processes and resource types: ";
-    cin >> n >> m;
-    cout<<"allocation matrix:\n";
-    for(int i=0;i<n;i++)
-        for(int j=0;j<m;j++)
-        cin>>Alloc[i][j];
+    cout << "Processes: "; cin >> n;
+    cout << "Resources: "; cin >> m;
 
-     cout << "Max Matrix:\n";
-    for (int i = 0; i < n; i++)
+    cout << "Allocation:\n";
+    for (int i = 0; i < n; i++) 
+        for (int j = 0; j < m; j++) 
+            cin >> A[i][j];
+
+    cout << "Max:\n";
+    for (int i = 0; i < n; i++) 
         for (int j = 0; j < m; j++) {
-            cin >> Max[i][j];
-            if (Alloc[i][j] > Max[i][j]) return cout << "Error at P" << i << " R" << j << endl, -1;
-            Need[i][j] = Max[i][j] - Alloc[i][j];
+            cin >> M[i][j];
+            if (A[i][j] > M[i][j]) {
+                cout << "Error at P" << i << " R" << j << endl;
+                return -1;
+            }
+            Need[i][j] = M[i][j] - A[i][j];
         }
-        cout<<"available resources: ";
-        for(int i=0;i<m;i++) cin>>Avail[i];
 
-        int count = 0;
-        while  (count<n){
-            bool progress = false;
-            for(int i=0;i<n;i++){
-                if (!Finish[i]) {
+    cout << "Available:\n";
+    for (int i = 0; i < m; i++) cin >> Avail[i];
+
+    int count = 0;
+    while (count < n) {
+        bool progress = false;
+        for (int i = 0; i < n; i++) {
+            if (!Finish[i]) {
                 bool canRun = true;
                 for (int j = 0; j < m; j++)
-                    if (Need[i][j] > Avail[j]) canRun = false;
+                    if (Need[i][j] > Avail[j]) {
+                        canRun = false;
+                        break;
+                    }
                 if (canRun) {
-                    for (int j = 0; j < m; j++) Avail[j] += Alloc[i][j];
-                    SafeSeq[count++] = i;
-                    Finish[i] = 1;
+                    for (int j = 0; j < m; j++) Avail[j] += A[i][j];
+                    Seq[count++] = i;
+                    Finish[i] = true;
                     progress = true;
                 }
             }
         }
         if (!progress) break;
-        }
-        if(count == n){
-            cout<<"Safe.Sequence:";
-            for (int i = 0; i < n; i++) cout << "P" << SafeSeq[i] << " ";
+    }
 
-        }else{
-        cout<<"Deadlock!Process:";
-        for(int i=0;i<n;i++) if(!Finish[i]) cout<< "p" <<i<<" ";
-        }
-        return 0;
-
+    if (count == n) {
+        cout << "SAFE: ";
+        for (int i = 0; i < n; i++) cout << "P" << Seq[i] << " ";
+        cout << endl;
+    } else {
+        cout << "DEADLOCK: ";
+        for (int i = 0; i < n; i++)
+            if (!Finish[i]) cout << "P" << i << " ";
+        cout << endl;
+    }
+    return 0;
 }
